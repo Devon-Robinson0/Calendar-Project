d028dc3314761c907966aaa4fa9b1f83efca57bb
Devon-Robinson0 <devon-robinson2@live.nmit.ac.nz>
2026-07-24 22:41:40 +1200

    CHANGE color and formatting

--- src/js/date-page.js
+++ src/js/date-page.js
@@ -52,15 +52,19 @@ function updateEvents() {
     for (const event of eventsOnDate) {
         const template = `
         <section id="event-${event.id}" class="event">
-            ${event.id}
-            <h2>${event.title}</h2>
-            <h3>Time: ${event.time || "_"}</h3>
-            <h3>location: ${event.location || "_"}</h3>
-            <p>${event.description || "_"}</p>
-
+            <div class="urgency-status low">
+            </div>
             <div>
-                <button class="edit-event-btn">Edit</button>
-                <button class="del-event-btn">Del</button>
+                ${event.id}
+                <h2>${event.title}</h2>
+                <h3>Time: ${event.time || "_"}</h3>
+                <h3>location: ${event.location || "_"}</h3>
+                <p>${event.description || "_"}</p>
+            </div>
+
+            <div class="event-btn-container">
+                <button class="edit-event-btn"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg></button>
+                <button class="del-event-btn"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg></button>
             </div
         </section>`
 
